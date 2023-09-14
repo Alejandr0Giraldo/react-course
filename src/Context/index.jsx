5e0694e6300e167953d14c45f51c8652b41d2500
Diego Giraldo <dgiraldogaleano@gmail.com>
2023-09-14 15:46:44 -0500

    mostrar el product detail e insertar la funcion de cerrar ventana

--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -3,10 +3,18 @@ import { createContext, useState } from 'react'
 export const ShoppingCartContext = createContext()
 
 export const ShoppingCartProvider = ({children}) => {
+    // Shopping cart . increment quantity
     const [count, setCount] = useState(0)
+
+    // Product Detail . open / close 
     const [isProductDetailOpen, setIsProductDetailOpen] = useState(false)
     const openProductDetail = () => setIsProductDetailOpen(true)
     const closeProductDetail = () => setIsProductDetailOpen(false)
+
+    // Product Detail . Show product 
+    const [productToShow, setProductToShow] = useState({})
+
+
     return (
         <ShoppingCartContext.Provider value={{
             count,
@@ -14,6 +22,8 @@ export const ShoppingCartProvider = ({children}) => {
             openProductDetail,
             closeProductDetail,
             isProductDetailOpen,
+            productToShow,
+            setProductToShow
         }}>
             {children}
         </ShoppingCartContext.Provider>
